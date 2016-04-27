4a7369a83889976633d2ec25c1ecb8987c9510bd
Beautiful Person! <Beautiful Person!>
2016-04-27 18:49:00 -0500

    Initial commit

---
+++ src/Book.java
@@ -0,0 +1,58 @@
+
+public class Book {
+	
+	private String title, author, publisher;
+	private int copy;
+	
+	
+	public Book()
+	{
+		
+	}
+	
+	public int getCopy() {
+		return copy;
+	}
+
+
+	public void setCopy(int copy) {
+		this.copy = copy;
+	}
+
+
+	
+
+	
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getAuthor() {
+		return author;
+	}
+
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+
+	
+
+}
