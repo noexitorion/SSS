c8cdfff288d04be2d6888c4baa76f833a3fb4f3c
Kevin Hinterlong <kevinhinterlong@gmail.com>
2016-05-11 14:45:58 -0500

    Removed commented methods from author

--- src/Book.java
+++ src/Book.java
@@ -108,27 +108,5 @@ public class Book {
 		this.authors = authors;
 	}
 
-	/*
-	public String getAuthor() {
-		return author;
-	}
-
-
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
-
-	public String getPublisher() {
-		return publisher;
-	}
-
-
-	public void setPublisher(String publisher) {
-		this.publisher = publisher;
-	}
-	 */
-
-	
 
 }
