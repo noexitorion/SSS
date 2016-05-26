5c0c84c2c9729f1bb7e6a7895b83aa3adfba2592
noexitorion <undyinggnostic@gmail.com>
2016-05-25 22:21:25 -0500

    Update LibraryViewer.java

--- src/LibraryViewer.java
+++ src/LibraryViewer.java
@@ -1,17 +1,14 @@
 import java.awt.Graphics;
 
-import javax.swing.JApplet;
-
-/**
- * 
- */
-
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
 /**
  * @author khinterlong
  *
  */
-public class LibraryViewer extends JApplet {
-
+public class LibraryViewer extends JApplet  {
 	
 	/**
 	 * 
@@ -21,15 +18,48 @@ public class LibraryViewer extends JApplet {
 	/**
 	 * 
 	 */
+	
+
 	public LibraryViewer() {
-		// TODO Auto-generated constructor stub
+		createFrame();
+		
+	
 	}
+	
 	
 	public void init() {
 		
 	}
+	
+	public static void createFrame()
+	{
+		JFrame libraryFrame = new JFrame("Library Checkout");
+		
+		libraryFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel emptyLabel = new JLabel("");   
+		emptyLabel.setPreferredSize(new Dimension(500, 500));
+		libraryFrame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+		libraryFrame.add(new textReader());
+
+		libraryFrame.pack();
+		libraryFrame.setVisible(true);	
+	}
+	public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createFrame();
+            }
+        });
+    }
+		
+		
+
 
 	public void paint(Graphics g) {
 		g.draw3DRect(0, 0, 100, 100, false);
 	}
+	
+	  
+	
+	
 }
